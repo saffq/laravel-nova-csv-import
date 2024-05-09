ee8811f50651e48119e37aaee3f937639a69edae
saffq <krystian.stepien2@wp.pl>
2024-05-09 11:28:09 +0200

    muliply column value

---
+++ src/Modifiers/MultiplyValue.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SimonHamp\LaravelNovaCsvImport\Modifiers;
+
+use SimonHamp\LaravelNovaCsvImport\Contracts\Modifier;
+
+class MultiplyValue implements Modifier
+{
+    public function title(): string
+    {
+        return 'Multiply Value';
+    }
+
+    public function description(): string
+    {
+        return 'Multiply the column value by a specified number';
+    }
+
+    public function settings(): array
+    {
+        return [
+            'multiplier' => [
+                'type' => 'number',
+                'title' => 'Multiplier',
+            ],
+        ];
+    }
+
+    public function handle($value = null, array $settings = [])
+    {
+        $multiplier = $settings['multiplier'] ?? 1;
+        if(is_numeric($value)) {
+            return $value * $multiplier;
+        } else {
+            return $value;
+        }
+    }
+}
